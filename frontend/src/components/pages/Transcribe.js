76602974e0436c392465462806faa09eeea0dee4
Mubashir Imran <mubashirimran@hotmail.com>
2024-07-21 22:41:04 +1000

    transcribe

--- frontend/src/components/pages/Transcribe.js
+++ frontend/src/components/pages/Transcribe.js
@@ -1,20 +1,19 @@
-// Transcribe.js
 import React, { useState, useRef, useEffect } from 'react';
 import { Box, Button, Typography, TextField, Snackbar, Alert, IconButton } from '@mui/material';
 import MicIcon from '@mui/icons-material/Mic';
 import StopIcon from '@mui/icons-material/Stop';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import axios from 'axios';
-import { encryptData, decryptData } from '../utils/encryption'; // Use encryption utilities
-import CONFIG from '../../.config'; // Import the config
+import { encryptData, decryptData } from '../utils/encryption';
+import CONFIG from '../../.config';
 import '../styles/Transcribe.css';
-import { useTranscription } from './TranscriptionContext'; // Import the context hook
+import { useTranscription } from './TranscriptionContext';
 
 const Transcribe = () => {
   const { transcription, setTranscription, transcriptionHistory, setTranscriptionHistory } = useTranscription();
   const [isRecording, setIsRecording] = useState(false);
-  const [counter, setCounter] = useState(0); // Define counter and setCounter
-  const [intervalTime, setIntervalTime] = useState(30); // Interval time state in seconds
+  const [counter, setCounter] = useState(0);
+  const [intervalTime, setIntervalTime] = useState(30);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarSeverity, setSnackbarSeverity] = useState('success');
@@ -23,15 +22,23 @@ const Transcribe = () => {
   const counterIntervalIdRef = useRef(null);
   const streamRef = useRef(null);
   const textareaRef = useRef(null);
-  const recordingIntervalRef = useRef(null); // Interval for recordings
+  const recordingIntervalRef = useRef(null);
 
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
   };
 
+  useEffect(() => {
+    return () => {
+      if (isRecording) {
+        stopRecording();
+      }
+    };
+  }, [isRecording]);
+
   const startRecording = () => {
     setIsRecording(true);
-    setCounter(0); // Reset counter when recording starts
+    setCounter(0);
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
       streamRef.current = stream;
       const mediaRecorder = new MediaRecorder(stream);
@@ -44,9 +51,8 @@ const Transcribe = () => {
       mediaRecorder.onstop = async () => {
         if (audioChunksRef.current.length > 0) {
           await sendAudioChunks();
-          audioChunksRef.current = []; // Clear the cache after sending chunks
+          audioChunksRef.current = [];
         }
-        // Restart the recording process if still recording
         if (isRecording) {
           mediaRecorder.start();
         }
@@ -58,28 +64,31 @@ const Transcribe = () => {
         setCounter((prevCounter) => prevCounter + 1);
       }, 1000);
 
-      // Set an interval to stop and start recording every intervalTime seconds
       recordingIntervalRef.current = setInterval(() => {
         if (mediaRecorderRef.current && mediaRecorderRef.current.state !== 'inactive') {
           mediaRecorderRef.current.stop();
           mediaRecorderRef.current.start();
         }
-      }, intervalTime * 1000); // Interval in milliseconds
+      }, intervalTime * 1000);
     });
   };
 
-  const stopRecording = () => {
+  const stopRecording = async () => {
     setIsRecording(false);
-    clearInterval(counterIntervalIdRef.current); // Clear counter interval
-    clearInterval(recordingIntervalRef.current); // Clear recording interval
+    clearInterval(counterIntervalIdRef.current);
+    clearInterval(recordingIntervalRef.current);
     if (mediaRecorderRef.current && mediaRecorderRef.current.state !== 'inactive') {
       mediaRecorderRef.current.stop();
     }
     if (streamRef.current) {
       streamRef.current.getTracks().forEach(track => track.stop());
     }
-    mediaRecorderRef.current = null; // Clear the mediaRecorderRef
-    streamRef.current = null; // Clear the streamRef
+    mediaRecorderRef.current = null;
+    streamRef.current = null;
+
+    if (audioChunksRef.current.length > 0) {
+      await sendAudioChunks();
+    }
   };
 
   const sendAudioChunks = async () => {
@@ -89,7 +98,7 @@ const Transcribe = () => {
     try {
       const formData = new FormData();
       formData.append('audio', audioBlob, 'audio.wav');
-      formData.append('counter', counter); // Send counter time to server
+      formData.append('counter', counter);
 
       const response = await axios.post(`${CONFIG.SERVER_IP}/api/transcribe`, formData, {
         headers: {
@@ -130,17 +139,20 @@ const Transcribe = () => {
   };
 
   const appendTranscription = (newText) => {
-    newText = removeRepetitions(newText); // Remove repetitions before appending
+    newText = removeRepetitions(newText);
 
     setTranscription((prev) => {
       const updatedText = `${prev}\n${newText}`;
       setTranscriptionHistory(updatedText);
+
+      // Save current cursor position
       if (textareaRef.current) {
         const { selectionStart, selectionEnd } = textareaRef.current;
         setTimeout(() => {
           textareaRef.current.setSelectionRange(selectionStart, selectionEnd);
         }, 0);
       }
+
       return updatedText;
     });
   };
@@ -211,15 +223,15 @@ const Transcribe = () => {
         Clear
       </Button>
       <Box sx={{ position: 'relative', flexGrow: 1, width: '100%' }}>
-        <TextField
-          inputRef={textareaRef}
-          className="transcribe-textarea"
-          variant="outlined"
-          multiline
-          value={transcription}
-          onChange={handleTranscriptionChange}
-          sx={{ width: '100%', height: '100%', resize: 'none' }}
-        />
+            <TextField
+        inputRef={textareaRef}
+        className="transcribe-textarea"
+        variant="outlined"
+        multiline
+        value={transcription}
+        onChange={handleTranscriptionChange}
+        sx={{ width: '90%', height: '450px', resize: 'none', overflow: 'auto' }}
+      />
         <Button
           variant="contained"
           color="primary"
@@ -231,18 +243,30 @@ const Transcribe = () => {
         </Button>
       </Box>
 
-      <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+
+
+      <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px', backgroundColor: 'white' }}>
         <Typography variant="body1" sx={{ marginRight: '10px' }}>Interval Time (sec):</Typography>
         <TextField
           type="number"
           value={intervalTime}
           onChange={handleIntervalChange}
-          inputProps={{ min: 5, step: 1 }}
+          inputProps={{ min: 1, step: 1 }}
           sx={{ width: '100px', marginRight: '10px' }}
         />
         <Button variant="contained" onClick={increaseInterval} sx={{ marginRight: '5px' }}>▲</Button>
         <Button variant="contained" onClick={decreaseInterval}>▼</Button>
       </Box>
+
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={stopRecording}
+        sx={{ position: 'relative',marginTop:'10px', bottom: 10, width: '20%', backgroundColor: 'red', color: 'white' }}
+      >
+        Stop Recording
+      </Button>
+      
 
       <Snackbar open={snackbarOpen} autoHideDuration={2000} onClose={handleSnackbarClose}>
         <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
